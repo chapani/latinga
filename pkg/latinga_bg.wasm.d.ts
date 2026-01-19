c3eb912f0f82bd8d71db843eb64f971e9db8fbed
chapani <chapani@users.noreply.github.com>
2026-01-19 18:41:35 +0000

    deploy: 6681469c68eebb6d25f96985b52e8120b9b4615c

---
+++ pkg/latinga_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_latinga_free: (a: number, b: number) => void;
+export const latinga_almashuvchilarni_yukla: (a: number, b: number, c: number) => void;
+export const latinga_atoqlilarni_yukla: (a: number, b: number, c: number) => void;
+export const latinga_oegir: (a: number, b: number, c: number, d: number) => void;
+export const latinga_qalqonlarni_yukla: (a: number, b: number, c: number) => number;
+export const latinga_tekshir: (a: number, b: number, c: number, d: number) => number;
+export const latinga_yangi: (a: number) => number;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
+export const __wbindgen_export3: (a: number, b: number, c: number) => void;
